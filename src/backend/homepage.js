e46657e83a3dd23c9f7765ec15f4f05fd6e6a36e
APURV CHOUBEY <apurv.main2@gmail.com>
2022-08-15 19:08:55 +0530

    updated README

--- src/backend/homepage.js
+++ src/backend/homepage.js
@@ -1,5 +1,25 @@
+let bgRed = true;
 const changeDivColor = () => {
     let element = document.getElementById("changeDivColor");
-    element.classList.toggle("bg-yellow-700");
-    console.log("working")
+    if(bgRed === true){
+        element.classList.add("bg-yellow-400");
+        element.classList.remove("bg-red-700");
+        // console.log("yellow");
+        bgRed = false;
+    }
+    else{
+        element.classList.add("bg-red-700");
+        element.classList.remove("bg-yellow-400");
+        // console.log("red");
+        bgRed = true;
+    }
+    // console.log("working");
+}
+
+// js for custom css
+const customChange = () => {
+    let element2 = document.getElementById("customChangeColor");
+    element2.classList.toggle("changeInGreen");
+    // console.log("working2");
+
 }
